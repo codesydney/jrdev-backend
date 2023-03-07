738b7b867c90bdf4bd58fb7ae0ed79f8293d9abb
NimaSakha <103934713+NimaSakha@users.noreply.github.com>
2023-03-07 19:46:07 +1100

    Add Cors() to server.js

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-require("dotenv").config()
-const express = require("express")
-const mongoose = require("mongoose")
-const port = process.env.PORT || 3004
-const app = express()
-const userRoutes = require("./routes/userRoutes")
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const port = process.env.PORT || 3004;
+const app = express();
+const userRoutes = require('./routes/userRoutes');
 
 //* middleware
-app.use(express.json())
+app.use(express.json());
 
-app.use("/api/user", userRoutes)
+app.use(cors());
+
+app.use('/api/user', userRoutes);
 
 //* connect to the DB
 mongoose
   .connect(process.env.DATABASE)
   .then(() => {
     app.listen(port, () => {
-      console.log(`Server running on port: ${port}`)
-    })
+      console.log(`Server running on port: ${port}`);
+    });
   })
-  .catch((error) => {
-    console.log(error)
-  })
+  .catch(error => {
+    console.log(error);
+  });
